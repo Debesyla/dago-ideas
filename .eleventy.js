6d415733ed2110f7ac8c09c5ed91ac5ff193f2d3
Debesyla <lopasada@gmail.com>
2025-09-23 22:31:50 +0300

    notbyai.fyi

--- .eleventy.js
+++ .eleventy.js
@@ -52,7 +52,7 @@ module.exports = function (eleventyConfig) {
   });
     
   eleventyConfig.addTemplateFormats("scss");
-
-  // Passthrough copy for favicon: place file at site root as /dago-icon.png
-  eleventyConfig.addPassthroughCopy({ "assets/img/dago-icon.png": "dago-icon.png" });
+  
+  // Passthrough copy for assets folder to preserve folder structure
+  // eleventyConfig.addPassthroughCopy("assets/img"); // <-- temp test
 };
